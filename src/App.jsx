c1c62e054bfdf8db4398a98ff687f8e59db422d7
LuiiVasquez <luivasque95@gmail.com>
2023-02-03 18:41:15 -0600

    adding product components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Card from "./components/Card";
+import ProductsForm from "./components/productsForm";
+import ProductsList from "./components/productsList";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <ProductsForm />
+      <ProductsList />
     </div>
   );
 }
