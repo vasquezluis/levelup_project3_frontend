0e633297cb6ab93fd161ff02184824c1652a3b88
LuiiVasquez <luivasque95@gmail.com>
2023-02-08 21:31:00 -0600

    adding routing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import "./App.css";
-import ProductsForm from "./components/productsForm";
-import ProductsList from "./components/productsList";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// * components
+import MainNavBar from "./components/_partials/MainNavBar";
+
+// * pages
+import Index from "./pages/public/Index";
+import Cartelera from "./pages/public/Cartelera";
+
+// * subpages
 
 function App() {
   return (
-    <div className="App">
-      <ProductsForm />
-      <ProductsList />
-    </div>
+    <BrowserRouter>
+      <div className="flex h-screen">
+        <div>
+          <MainNavBar />
+        </div>
+
+        <div className="p-2 w-screen h-screen">
+          <Routes>
+            <Route index element={<Index />} />
+            <Route path="/cartelera" element={<Cartelera />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
