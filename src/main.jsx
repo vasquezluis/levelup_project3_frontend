ef4097d6909bad44d2c0b6b1ffb2f8eac05dfedb
LuiiVasquez <luivasque95@gmail.com>
2023-02-16 12:36:29 -0600

    added contextProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { ContextProvider } from "./contexts/ContextProvider";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
@@ -10,9 +11,13 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    {/* Get all data insive provider */}
+    <ContextProvider>
+      {/* user react query component */}
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ContextProvider>
   </React.StrictMode>
 );
