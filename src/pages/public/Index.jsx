2dca015671c71071170f86bc74ad41c2d99c832e
LuiiVasquez <luivasque95@gmail.com>
2023-02-17 16:30:58 -0600

    changing layout classes

--- src/pages/public/Index.jsx
+++ src/pages/public/Index.jsx
@@ -35,87 +35,100 @@ function Index() {
   const movieToShow2 = randomMovie();
 
   return (
-    <div className="h-full w-full">
-      <div className="bg-main-dark-bg flex flex-col w-full items-center py-5">
-        <p className="text-4xl font-bold ">Peliculas destacadas</p>
-        <p className="py-3">
-          Cada semana te traemos las mejores peliculas internacionales del
-          momento
-        </p>
-      </div>
-      {/* top part */}
-      <div className="w-full mx-auto py-10 bg-gray-900 grid lg:grid-cols-2 gap-5">
-        {/* Poster  */}
-        <div className="p-5 pl-10 flex">
-          <img
-            className="w-[50%]"
-            src={movieToShow.poster}
-            alt={movieToShow.name}
-          />
-          <div className="flex flex-col pl-10 pt-10 max-w-sm">
-            <h2 className=" text-3xl font-bold mb-4">{movieToShow.name}</h2>
-            <p className="mb-4">
-              Sinopsis: {movieToShow.description.substring(0, 200)}...
-            </p>
-            <p>
-              Genero:
-              {movieToShow.genders.map((item, index) => (
-                <span
-                  className="text-gray-900 bg-gray-200 px-1 mx-1 rounded-md"
-                  key={index}
-                >
-                  {item}
-                </span>
-              ))}
-            </p>
+    <div className="mt-5">
+      <div className="flex flex-wrap lg:flex-wrap justify-center">
+        {/* Layout */}
+        {/* <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p>Earnings</p>
+              <p>Hola</p>
+            </div>
           </div>
-        </div>
-        {/* Poster  */}
-        <div className="p-5 pl-10 flex">
-          <img
-            className="w-[50%]"
-            src={movieToShow2.poster}
-            alt={movieToShow2.name}
-          />
-          <div className="flex flex-col pl-10 pt-10 max-w-sm">
-            <h2 className="text-3xl font-bold mb-4">{movieToShow2.name}</h2>
-            <p className="mb-4">
-              Sinopsis: {movieToShow2.description.substring(0, 200)}...
-            </p>
-            <p>
-              Genero:
-              {movieToShow2.genders.map((item, index) => (
-                <span
-                  className="text-gray-900 bg-gray-200 px-1 mx-1 rounded-md"
-                  key={index}
-                >
-                  {item}
-                </span>
-              ))}
-            </p>
+          <div className="mt-6">
+            <button>Hola</button>
           </div>
+        </div> */}
+
+        {/* new section */}
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          <p className="text-3xl font-bold">Peliculas destacadas</p>
         </div>
-      </div>
-      {/* below part */}
-      <div className="text-center bg-gray-900 ">
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            consequuntur nemo labore laborum enim itaque dolores sed, ad
-            ratione! Voluptates aliquam sequi ipsa eum praesentium, tempora
-            maxime temporibus assumenda quas? Aut natus magni modi, provident a
-            magnam ipsam sed dicta doloribus fugiat inventore, aliquid amet
-            incidunt itaque sapiente soluta nihil autem voluptatem repudiandae
-            illum cum! Dolorum eveniet molestiae soluta placeat.
-          </p>
-        </div>
-        <div className="my-3 pb-5 mt-4">
-          <Link
-            className="bg-burgundy hover:bg-red-700 font-bold py-2 px-4 rounded-full mb-4"
-            to="/cartelera"
-          >
-            Ver carteleras
-          </Link>
+
+        {/* new section */}
+        <div className="flex m-3 bg-white flex-wrap justify-center gap1 items-center">
+          {/* top part */}
+          <div className="w-full mx-auto py-10 grid lg:grid-cols-2 gap-5">
+            {/* Poster  */}
+            <div className="p-5 pl-10 flex">
+              <img
+                className="w-[50%]"
+                src={movieToShow.poster}
+                alt={movieToShow.name}
+              />
+              <div className="flex flex-col pl-10 pt-10 max-w-sm">
+                <h2 className=" text-3xl font-bold mb-4">{movieToShow.name}</h2>
+                <p className="mb-4">
+                  Sinopsis: {movieToShow.description.substring(0, 200)}...
+                </p>
+                <p>
+                  Genero:
+                  {movieToShow.genders.map((item, index) => (
+                    <span
+                      className="bg-gray-200 px-1 mx-1 rounded-md"
+                      key={index}
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
+            {/* Poster  */}
+            <div className="p-5 pl-10 flex">
+              <img
+                className="w-[50%]"
+                src={movieToShow2.poster}
+                alt={movieToShow2.name}
+              />
+              <div className="flex flex-col pl-10 pt-10 max-w-sm">
+                <h2 className="text-3xl font-bold mb-4">{movieToShow2.name}</h2>
+                <p className="mb-4">
+                  Sinopsis: {movieToShow2.description.substring(0, 200)}...
+                </p>
+                <p>
+                  Genero:
+                  {movieToShow2.genders.map((item, index) => (
+                    <span
+                      className="text-gray-900 bg-gray-200 px-1 mx-1 rounded-md"
+                      key={index}
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* below part */}
+          <div className="text-center bg-white">
+            <div>
+              <p>
+                <span className="font-bold">Cines de Guatemala</span> es una cadena de cines que te
+                ofrecen las mejores peliculas, los mejores estrenos, y las
+                mejores salas de cines del país. Descubre la variedad de
+                peliculas disponibles!
+              </p>
+            </div>
+            <div className="my-3 pb-5 mt-4">
+              <Link
+                className="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full mb-4"
+                to="/cartelera"
+              >
+                Ver carteleras
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
