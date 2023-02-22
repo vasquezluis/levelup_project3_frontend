79b9c636dc179e498729d05746d39cecd1cbf93c
LuiiVasquez <luivasque95@gmail.com>
2023-02-21 23:19:47 -0600

    created API users

---
+++ src/api/usersAPI.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+// axios instance
+
+const usersAPI = axios.create({
+  baseURL: "http://localhost:3000/api/v1/users",
+});
+
+/**
+ * * endpoints functions
+ */
+
+export const getItem = async (id) => {
+  const response = await usersAPI.get(`/${id}`);
+
+  return response.data.body;
+};
+
+export const getItems = async () => {
+  const response = await usersAPI.get("/");
+
+  return response.data.body;
+};
+
+export const getReservations = async (userId) => {
+  const response = await usersAPI.get(`/${userId}/reservations`);
+
+  return response.data.body;
+};
