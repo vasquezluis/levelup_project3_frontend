d192fcf9785adb631b5936f88fd0741353c11846
LuiiVasquez <luivasque95@gmail.com>
2023-02-24 22:22:30 -0600

    added connection to api on onSubmit function

--- src/components/users/SignInUserForm.jsx
+++ src/components/users/SignInUserForm.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import { useRef } from "react";
+import { authFunction } from "../../api/authAPI";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignInUserForm() {
+  const formRef = useRef();
+  const navigate = useNavigate();
+  const [passwordMessage, setPasswordMessage] = useState(false);
+  const [userMessage, setUserMessage] = useState(false);
+
+  const loginMutation = useMutation({
+    mutationFn: authFunction,
+    onSuccess: () => {
+      navigate("/userdash");
+    },
+    onError: (error, variables, context) => {
+      if (error.response.status == 404) {
+        setUserMessage(true);
+        setPasswordMessage(false);
+      } else if (error.response.status == 401) {
+        setPasswordMessage(true);
+        setUserMessage(true);
+      }
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target); // ? getting form data
+    const loginData = Object.fromEntries(formData); // ? creatind product object from formData
+
+    loginMutation.mutate(loginData);
+
+    formRef.current.reset();
+  };
+
   return (
     <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+        ref={formRef}
+      >
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -12,11 +52,16 @@ function SignInUserForm() {
             Username
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none mb-3 focus:shadow-outline"
             id="username"
+            name="user"
             type="text"
             placeholder="Username"
+            required
           />
+          <p className="text-red-500 text-xs italic">
+            {userMessage ? "Usuario no encontrado" : ""}
+          </p>
         </div>
         <div className="mb-6">
           <label
@@ -28,31 +73,21 @@ function SignInUserForm() {
           <input
             className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
+            name="password"
             type="password"
-            placeholder="******************"
+            placeholder="******"
+            required
           />
           <p className="text-red-500 text-xs italic">
-            Please choose a password.
+            {passwordMessage ? "Contraseña incorrecta" : ""}
           </p>
         </div>
         <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Sign In
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Login
           </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
       </form>
-      <p className="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
-      </p>
     </div>
   );
 }
