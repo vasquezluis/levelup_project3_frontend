92c2f12881fa0cd97afeafd08431519802352bca
LuiiVasquez <luivasque95@gmail.com>
2023-02-25 14:24:44 -0600

    used new axios config

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,35 +1,29 @@
-import axios from "axios";
-
-// axios instance
-
-const usersAPI = axios.create({
-  baseURL: "http://localhost:3000/api/v1/users",
-});
+import axios from "../libs/axios.js";
 
 /**
  * * endpoints functions
  */
 
 export const getItem = async (id) => {
-  const response = await usersAPI.get(`/${id}`);
+  const response = await axios.get(`/users/${id}`);
 
   return response.data.body;
 };
 
 export const getItems = async () => {
-  const response = await usersAPI.get("/");
+  const response = await axios.get("/users");
 
   return response.data.body;
 };
 
 export const getReservations = async (userId) => {
-  const response = await usersAPI.get(`/${userId}/reservations`);
+  const response = await axios.get(`/users/${userId}/reservations`);
 
   return response.data.body;
 };
 
 export const createItem = async (body) => {
-  const response = await usersAPI.post("/", body);
+  const response = await axios.post("/users/", body);
 
   return response.data.body;
 };
