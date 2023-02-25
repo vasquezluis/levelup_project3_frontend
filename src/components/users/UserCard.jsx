e1593997d7f879da8f700b3e251a62f68ee3b126
LuiiVasquez <luivasque95@gmail.com>
2023-02-24 22:21:11 -0600

    added name, lastname and roles to card

--- src/components/users/UserCard.jsx
+++ src/components/users/UserCard.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-function UserCard({ user, credits, identification, email }) {
+function UserCard({
+  user,
+  name,
+  roles,
+  lastname,
+  credits,
+  identification,
+  email,
+}) {
   return (
     <div className="w-60 rounded overflow-hidden shadow-lg p-2 m-1 bg-gray-100 text-black">
       <p>Usuario: {user}</p>
+      <p>Nombres: {name}</p>
+      <p>Apellidos: {lastname}</p>
       <p>Creditos: {credits}</p>
       <p>ID: {identification}</p>
       <p>Email: {email}</p>
+      <p>
+        Rol:{" "}
+        {roles.map((item, index) => (
+          <span key={index}>{item}</span>
+        ))}
+      </p>
     </div>
   );
 }
