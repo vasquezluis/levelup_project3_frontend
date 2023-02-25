06f8261d0e9e4c1d3ec2374853e018e12a212070
LuiiVasquez <luivasque95@gmail.com>
2023-02-24 22:23:19 -0600

    added tailwind classes to p tags

--- src/components/reservations/ReservationCard.jsx
+++ src/components/reservations/ReservationCard.jsx
@@ -10,23 +10,24 @@ function ReservationCard({
   cinema,
 }) {
   return (
-    <div className="max-w-xs rounded shadow-xl p-2 m-1 bg-gray-50 text-black">
+    <div className="lg:w-44 rounded shadow-xl p-2 m-1 bg-gray-50 text-black">
       <div className="flex flex-col items-center">
         <img className="w-28" src={moviePoster} alt={movie} />
       </div>
-      <p className="text-xl font-bold mt-1">{movie}</p>
-      <p>Total creditos: {totalCredits}</p>
-      <p className="max-w-xs">
-        Asientos:
+      <p className="mt-1">{movie}</p>
+      <p className="text-xs">Total creditos: {totalCredits}</p>
+      <div className="flex flex-grow-0 w-10">
+        <p className="text-xs">Asientos:</p>
+
         {seats.map((item, index) => (
-          <span key={index} className="bg-gray-300 rounded-md px-2 mx-1">
+          <span key={index} className="bg-gray-300 rounded-md px-1 mx-1 text-xs">
             {item}
           </span>
         ))}
-      </p>
-      <p>Horario: {schedule}</p>
-      <p>Fecha: {dateFormat(date, "shortDate")}</p>
-      <p>Sala: {cinema}</p>
+      </div>
+      <p className="text-xs font-bold">Horario: {schedule}</p>
+      <p className="text-xs font-bold">Fecha: {dateFormat(date, "shortDate")}</p>
+      <p className="text-xs font-bold">Sala: {cinema}</p>
     </div>
   );
 }
