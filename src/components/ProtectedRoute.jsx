0f360a27d287d7b9d02e6f37310f125d321d7395
LuiiVasquez <luivasque95@gmail.com>
2023-02-27 15:28:28 -0600

    removed console log debugger

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,8 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 function ProtectedRoute({ isAllowed, redirecTo = "/", children }) {
   if (!isAllowed) return <Navigate to={redirecTo} />;
 
-  console.log(isAllowed);
-
   return children ? children : <Outlet />;
 }
 
