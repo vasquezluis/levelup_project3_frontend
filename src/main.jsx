db0c56743fe1316160db4dfa280f200e973c7ec6
LuiiVasquez <luivasque95@gmail.com>
2023-02-27 15:08:24 -0600

    added redux store to provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { ContextProvider } from "./contexts/ContextProvider";
+// ? react query
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+
+// ? redux
+import { Provider } from "react-redux";
+import store from "./app/store.js";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* Get all data insive provider */}
-    <ContextProvider>
+    <Provider store={store}>
       {/* user react query component */}
       <QueryClientProvider client={queryClient}>
         <App />
         <ReactQueryDevtools />
       </QueryClientProvider>
-    </ContextProvider>
+    </Provider>
   </React.StrictMode>
 );
