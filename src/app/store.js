05771cbc3b92b8414adc7eb6a0d60d157be88c61
LuiiVasquez <luivasque95@gmail.com>
2023-02-27 15:28:51 -0600

    added user reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import themeReducer from "../features/theme/themeSlice";
 
-export const store = configureStore({
+import themeReducer from "../features/theme/themeSlice";
+import userReducer from "../reducers/userSlice";
+
+export default configureStore({
   reducer: {
     theme: themeReducer,
+    user: userReducer,
   },
 });
