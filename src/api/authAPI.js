0639b872f54794661e76dd2b6a4f7d433bff96b4
LuiiVasquez <luivasque95@gmail.com>
2023-02-27 15:29:17 -0600

    changed baseURL

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios instance
 
 const authAPI = axios.create({
-  baseURL: "http://localhost:3000/api/v1/auth",
+  baseURL: "http://localhost:3000/api/v1/login",
 });
 
 /**
@@ -13,5 +13,5 @@ const authAPI = axios.create({
 export const authFunction = async (body) => {
   const response = await authAPI.post("/", body);
 
-  return response.data.body;
+  return response.data;
 };
